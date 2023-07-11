ae9465747dd4cc952404832f37c38669a9d89f99
Domenico <domenicofaccilongo@gmail.com>
2023-07-11 12:48:48 +0200

    #1 modifica

--- Progetto_pokemon/src/app/app.module.ts
+++ Progetto_pokemon/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { HeaderComponent } from './header/header.component';
+import { AutenticazioneComponent } from './header/autenticazione/autenticazione.component';
+import { CommonModule } from '@angular/common';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
+import { DividerModule } from "primeng/divider";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AutenticazioneComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    CommonModule,
+    ButtonModule,
+    PasswordModule,
+    DividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
